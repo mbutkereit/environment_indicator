dfe143b4fdb77f7358634918da7a54de75497d2d
Tom Kirkpatrick <tom@systemseed.com>
2013-07-01 23:32:31 +0200

    Ability to enable/disable environment indicators

--- lib/Drupal/environment_indicator/EnvironmentIndicatorListController.php
+++ lib/Drupal/environment_indicator/EnvironmentIndicatorListController.php
@@ -16,12 +16,41 @@ use Drupal\Core\Entity\EntityInterface;
 class EnvironmentIndicatorListController extends ConfigEntityListController {
 
   /**
-   * Overrides Drupal\Core\Entity\EntityListController::getOperations().
+   * Overrides Drupal\Core\Entity\EntityListController::load();
    */
-  public function getOperations(EntityInterface $entity) {
-    $operations = parent::getOperations($entity);
-    return $operations;
+  public function load() {
+    $entities = array(
+      'enabled' => array(),
+      'disabled' => array(),
+    );
+    foreach (parent::load() as $entity) {
+      if ($entity->isEnabled()) {
+        $entities['enabled'][] = $entity;
+      }
+      else {
+        $entities['disabled'][] = $entity;
+      }
+    }
+    return $entities;
   }
+
+  /**
+   * Overrides Drupal\Core\Entity\EntityListController::buildRow();
+   */
+  public function buildRow(EntityInterface $entity) {
+    return array(
+      'data' => array(
+        'name' => $entity->get('human_name'),
+        'description' => $entity->get('description'),
+        'operations' => array(
+          'data' => $this->buildOperations($entity),
+        ),
+      ),
+      'title' => t('Machine name: ') . $entity->id(),
+      'class' => array($entity->isEnabled() ? 'views-ui-list-enabled' : 'views-ui-list-disabled'),
+    );
+  }
+
 
   /**
    * Overrides Drupal\Core\Entity\EntityListController::buildHeader().
@@ -34,14 +63,105 @@ class EnvironmentIndicatorListController extends ConfigEntityListController {
   }
 
   /**
-   * Overrides Drupal\Core\Entity\EntityListController::buildRow().
+   * Overrides Drupal\Core\Entity\EntityListController::getOperations().
    */
-  public function buildRow(EntityInterface $entity) {
-   // die(print_r($entity));
-    $row['name'] = check_plain($entity->human_name);
-    $row['description'] = check_plain($entity->description);
-    $row['operations']['data'] = $this->buildOperations($entity);
-    return $row;
+  public function getOperations(EntityInterface $entity) {
+    $uri = $entity->uri();
+    $path = $uri['path'];
+
+    $definition['edit'] = array(
+      'title' => t('Edit'),
+      'href' => "$path/edit",
+      'weight' => -5,
+    );
+    if (!$entity->isEnabled()) {
+      $definition['enable'] = array(
+        'title' => t('Enable'),
+        'ajax' => TRUE,
+        'token' => TRUE,
+        'href' => "$path/enable",
+        'weight' => -10,
+      );
+    }
+    else {
+      $definition['disable'] = array(
+        'title' => t('Disable'),
+        'ajax' => TRUE,
+        'token' => TRUE,
+        'href' => "$path/disable",
+        'weight' => 0,
+      );
+    }
+    // This property doesn't exist yet.
+    if (!empty($entity->overridden)) {
+      $definition['revert'] = array(
+        'title' => t('Revert'),
+        'href' => "$path/revert",
+        'weight' => 5,
+      );
+    }
+    else {
+      $definition['delete'] = array(
+        'title' => t('Delete'),
+        'href' => "$path/delete",
+        'weight' => 10,
+      );
+    }
+    return $definition;
   }
+
+
+  /**
+   * Overrides Drupal\Core\Entity\EntityListController::buildOperations();
+   */
+  public function buildOperations(EntityInterface $entity) {
+    $build = parent::buildOperations($entity);
+
+    // Allow operations to specify that they use AJAX.
+    foreach ($build['#links'] as &$operation) {
+      if (!empty($operation['ajax'])) {
+        $operation['attributes']['class'][] = 'use-ajax';
+      }
+    }
+
+    // Use the dropbutton #type.
+    unset($build['#theme']);
+    $build['#type'] = 'dropbutton';
+
+    return $build;
+  }
+
+  /**
+   * Overrides Drupal\Core\Entity\EntityListController::render();
+   */
+  public function render() {
+    $entities = $this->load();
+    $list['#type'] = 'container';
+    //$list['#attached']['css'] = ViewFormControllerBase::getAdminCSS();
+    $list['#attached']['library'][] = array('system', 'drupal.ajax');
+    $list['#attributes']['id'] = 'environment-indicator-entity-list';
+    $list['enabled']['heading']['#markup'] = '<h2>' . t('Enabled') . '</h2>';
+    $list['disabled']['heading']['#markup'] = '<h2>' . t('Disabled') . '</h2>';
+    foreach (array('enabled', 'disabled') as $status) {
+      $list[$status]['#type'] = 'container';
+      $list[$status]['#attributes'] = array('class' => array('views-list-section', $status));
+      $list[$status]['table'] = array(
+        '#theme' => 'table',
+        '#header' => $this->buildHeader(),
+        '#rows' => array(),
+      );
+      foreach ($entities[$status] as $entity) {
+        $list[$status]['table']['#rows'][$entity->id()] = $this->buildRow($entity);
+      }
+    }
+    // @todo Use a placeholder for the entity label if this is abstracted to
+    // other entity types.
+    $list['enabled']['table']['#empty'] = t('There are no enabled environment indicators.');
+    $list['disabled']['table']['#empty'] = t('There are no disabled environment indicators.');
+
+    return $list;
+  }
+  
+  
 
 }
